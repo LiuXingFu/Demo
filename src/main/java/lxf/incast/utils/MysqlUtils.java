4464544a626900e7a25ce5cfffc6f9378abc0824
2435788581@qq.com <39995556+LiuXingFu@users.noreply.github.com>
2019-03-06 22:42:07 +0800

    优化Mysql工具类，静态代码块加载jdbc.properties文件

--- src/main/java/lxf/incast/utils/MysqlUtils.java
+++ src/main/java/lxf/incast/utils/MysqlUtils.java
@@ -1,9 +1,12 @@
 package lxf.incast.utils;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 /**
  * mysql工具类
@@ -13,38 +16,109 @@ import java.sql.Statement;
  * @date 2019年3月4日 上午10:55:18
  */
 public class MysqlUtils {
+
+	private static String url = null;
+	private static String user = null;
+	private static String password = null;
+	private static String driverClass = null;
 	
-	private static Connection connection = null;
-	private static Statement statement = null;
-	private static String url = "jdbc:mysql://localhost:3306/test01";
-	private static String user = "root";
-	private static String password = "liu24357";
-
-	//返回一个Statement对象
-	public static Statement ReturnStatement(){
+	/**
+	 * 静态代码块，随着类的加载而加载
+	 */
+	static {
 		try {
-			// 注册驱动
-			Class.forName("com.mysql.jdbc.Driver");
-			// 获取链接
-			connection = DriverManager.getConnection(url, user, password);
-			// 创建Statement对象
-			statement = connection.createStatement();
-			// 返回Statement对象
-			return statement;
+			//读取jdbc.properties文件
+			Properties properties = new Properties();
+			//使用类路径方式读取配置文件
+			InputStream inputStream = MysqlUtils.class.getResourceAsStream("/jdbc.properties");
+			//加载文件
+			properties.load(inputStream);
+			url = properties.getProperty("url");
+			user = properties.getProperty("user");
+			password = properties.getProperty("password");
+			driverClass = properties.getProperty("driverClass");
+			//注册驱动
+			Class.forName(driverClass);
 		} catch (Exception e) {
+			// TODO: handle exception
 			e.printStackTrace();
-			System.out.println("出错了...");
 		}
-
-		return null;
 	}
-
-	public static void CloseConnection() {
+	
+	/**
+	 * 获取链接资源
+	 * @return connection
+	 */
+	public static Connection getConnection() {
 		try {
-			connection.close();
+			Connection connection = DriverManager.getConnection(url, user, password);
+			return connection;
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// TODO: handle exception
 			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * 关闭资源
+	 * @param statement 执行静态sql
+	 * @param connection 链接对象
+	 */
+	public static void close(Statement statement, Connection connection) {
+		if(statement != null){
+			try {
+				statement.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+		if(connection != null){
+			try {
+				connection.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	/**
+	 * 关闭资源
+	 * @param statement 执行静态sql
+	 * @param connection 链接对象
+	 * @param resultSet 结果集对象
+	 */
+	public static void close(Statement statement, Connection connection, ResultSet resultSet) {
+		if(resultSet != null){
+			try {
+				resultSet.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+		if(statement != null){
+			try {
+				statement.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
+		if(connection != null){
+			try {
+				connection.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
