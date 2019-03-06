42a8291a051189578daca07ec66e81252486ae50
2435788581@qq.com <39995556+LiuXingFu@users.noreply.github.com>
2019-03-06 22:56:03 +0800

    查询所有的访问数据用request域传到jsp页面显示

--- src/main/java/lxf/incast/web/IndexServlet.java
+++ src/main/java/lxf/incast/web/IndexServlet.java
@@ -1,10 +1,16 @@
 package lxf.incast.web;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import lxf.incast.entity.Record;
+import lxf.incast.service.RecordService;
+import lxf.incast.service.impl.RedcordServiceImpl;
 
 /**
  * 
@@ -14,11 +20,14 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class IndexServlet extends HttpServlet {
 
+	private RecordService recordService = new RedcordServiceImpl();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String index = "《《《欢迎来到本网站》》》";
+		List<Record> record_list = recordService.RecordAll();
 		request.setAttribute("index", index);
+		request.setAttribute("record_list", record_list);
 		request.getRequestDispatcher("WEB-INF/view/index.jsp").forward(request, response);
 	}
 
