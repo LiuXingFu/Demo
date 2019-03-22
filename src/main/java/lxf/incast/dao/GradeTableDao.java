85471dbbe33776a253c61eefa5faae54ce60f4dd
2435788581@qq.com <39995556+LiuXingFu@users.noreply.github.com>
2019-03-22 22:39:38 +0800

    1610持久层接口

---
+++ src/main/java/lxf/incast/dao/GradeTableDao.java
@@ -0,0 +1,16 @@
+package lxf.incast.dao;
+
+import java.util.List;
+
+import lxf.incast.entity.GradeTable;
+
+/**
+ * 1610成绩持久层接口
+ * @author 我有一个小梦想
+ * 
+ * @date 2019年3月22日 下午9:30:15
+ */
+public interface GradeTableDao {
+	//查询所有成绩
+	public List<GradeTable> getGradeTableList();
+}
