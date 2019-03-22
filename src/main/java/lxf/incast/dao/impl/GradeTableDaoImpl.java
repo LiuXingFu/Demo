5e1236b8bafca5ce2c70110fea3d6972de6cbdd2
2435788581@qq.com <39995556+LiuXingFu@users.noreply.github.com>
2019-03-22 22:40:10 +0800

    1610持久层实现类

---
+++ src/main/java/lxf/incast/dao/impl/GradeTableDaoImpl.java
@@ -0,0 +1,31 @@
+package lxf.incast.dao.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import lxf.incast.dao.GradeTableDao;
+import lxf.incast.entity.GradeTable;
+import lxf.incast.utils.SqlSessionFactorUtils;
+
+/**
+ * 1610成绩持久层实现类
+ * @author 我有一个小梦想
+ * 
+ * @date 2019年3月22日 下午9:32:07
+ */
+public class GradeTableDaoImpl implements GradeTableDao {
+	
+	private static SqlSession session = SqlSessionFactorUtils.getSqlSession();
+	private static GradeTableDao gradeTableDao = session.getMapper(GradeTableDao.class);
+
+	/*
+	 * 查询所有成绩(non-Javadoc)
+	 * @see lxf.incast.dao.GradeTableDao#getGradeTableList()
+	 */
+	public List<GradeTable> getGradeTableList() {
+		
+		return gradeTableDao.getGradeTableList();
+	}
+
+}
